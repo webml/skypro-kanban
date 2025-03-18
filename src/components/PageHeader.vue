eb0527d70e6d8a38412fccbd891eefa831c54603
Misha Lubarets <lumek@icloud.com>
2025-03-18 10:30:18 +0300

    first-hm

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HeaderNav from './HeaderNav.vue'
+</script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__block">
+        <div class="header__logo _show _light">
+          <a href="" target="_self"><img src="@/assets/images/logo.png" alt="logo" /></a>
+        </div>
+        <div class="header__logo _dark">
+          <a href="" target="_self"><img src="@/assets/images/logo_dark.png" alt="logo" /></a>
+        </div>
+        <HeaderNav />
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/ThreeDotsButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card__btn">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
