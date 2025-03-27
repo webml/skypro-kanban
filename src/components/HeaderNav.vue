81cf5ebdf81d3fb049474900f8c982b7e641bf2f
Misha Lubarets <lumek@icloud.com>
2025-03-27 13:10:37 +0300

    second-hm add user modal

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,22 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+import UserModal from './modals/UserModal.vue'
+
+const isShowModal = ref(false)
+const changeShowModal = () => {
+  isShowModal.value = !isShowModal.value
+}
+</script>
+
 <template>
   <nav class="header__nav">
     <button class="header__btn-main-new _hover01" id="btnMainNew">
       <a href="#popNewCard">Создать новую задачу</a>
     </button>
-    <a href="#user-set-target" class="header__user _hover02">Ivan Ivanov</a>
-    <div class="header__pop-user-set pop-user-set" id="user-set-target">
-      <!-- <a href="">x</a> -->
-      <p class="pop-user-set__name">Ivan Ivanov</p>
-      <p class="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-      <div class="pop-user-set__theme">
-        <p>Темная тема</p>
-        <input type="checkbox" class="checkbox" name="checkbox" />
-      </div>
-      <button type="button" class="_hover03"><a href="#popExit">Выйти</a></button>
-    </div>
+    <button class="header__user _hover02" @click="changeShowModal">Ivan Ivanov</button>
+    <UserModal v-if="isShowModal" />
   </nav>
 </template>
-
-<script setup></script>
 
 <style lang="scss" scoped></style>
